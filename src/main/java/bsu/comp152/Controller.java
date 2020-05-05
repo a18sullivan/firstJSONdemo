33ad14bbda59213ddf134a1e0f3d0b9f59d347a1
a18sullivan <a18sullivan@student.bridgew.edu>
2020-05-04 22:38:29 -0400

    a

--- src/main/java/bsu/comp152/Controller.java
+++ src/main/java/bsu/comp152/Controller.java
@@ -1,4 +1,23 @@
 package bsu.comp152;
 
-public class Controller {
-}
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+public class Controller implements Initializable {
+    @FXML
+    private ListView<DataHandler.recipeDataType> ListControl;
+    private DataHandler Model;
+    public void loadData(){
+        var site = "http://www.recipepuppy.com/api/?";
+        var params = getQueryParameters();
+        var query = site+params;
+        Model = new DataHandler(query);
+        var recipeList = Model.getData();
+        ObservableList<DataHandler.recipeDataType> dataToShow =
+                FXCollections.observableArrayList(recipeList);
+        ListControl.setItems(dataToShow);
+    }
+
--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,7 +1,13 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -11,6 +17,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Parent root = null;
+        URL loc = getClass().getResource("Main.fxml");
+        try{
+            root= FXMLLoader.load(loc);
+        }catch(IOException e){
+            System.out.println("Couldnt find FXML file");
+        }
+        Scene windowContents = new Scene(root, 300,400);
+        primaryStage.setScene(windowContents);
+        primaryStage.setTitle("Showing Web Data");
+        primaryStage.show();
     }
 }
